428c387cc7bba9ee374d8a298263d821dca62dff
Lipatova-Liza13 <lipatovaliza13@gmail.com>
2020-08-07 23:08:02 +0300

    create tab bar controller

---
+++ FirstGitProject/FirstGitProject/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  FirstGitProject
+//
+//  Created by Teslenko Anastasiya on 07.08.2020.
+//  Copyright © 2020 Liza Lipatova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+
+  
+
+
+}
+}
